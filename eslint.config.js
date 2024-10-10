34289b22cc4c4d6041024acbb036c48a4318e57f
Azat Khabirov <zt100rq@gmail.com>
2024-10-10 22:54:35 +0500

    шаблон готов

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   { ignores: ['dist'] },
@@ -22,17 +24,34 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
+      'eslint/prettier': eslintConfigPrettier,
     },
     rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'linebreak-style': [0, 'unix'],
+      quotes: ['error', 'single'],
+      semi: 0,
+      endOfLine: 'off',
+      'react/prop-types': 0,
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          parser: 'flow',
+          endOfLine: 'auto',
+        },
       ],
+      indent: ['error', 2],
+      'no-mixed-spaces-and-tabs': 0,
+      'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
-]
+];
