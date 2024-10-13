18ad9a461722e016f1fc5bd8d8d25e1152708b4b
Azat Khabirov <zt100rq@gmail.com>
2024-10-13 20:43:04 +0500

    commit for test

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,30 +1,91 @@
-import { Form, Input, Typography, Button } from 'antd';
+import { Form, Input, Typography, Button, Checkbox } from 'antd';
 import styles from '../SignUp/SignUp.module.scss';
-import { LockOutlined, MailOutlined } from '@ant-design/icons';
+import { LockOutlined, MailOutlined, UnlockOutlined, UserOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 const { Title } = Typography;
 
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not a valid email!',
+  },
+};
+
 function SignUp() {
+  const [form] = Form.useForm();
   return (
-    <Form layout="vertical" className={styles.Form}>
+    <Form
+      form={form}
+      layout="vertical"
+      name="register"
+      onFinish={(value) => console.log(value)}
+      className={styles.Form}
+      validateMessages={validateMessages}
+    >
       <Title className={styles.Title} level={4}>
         Create new account
       </Title>
-      <Form.Item label="Username:" name="username">
-        <Input placeholder="Username" size="large"></Input>
+      <Form.Item label="Username:" name="username" rules={[{ required: true, massage: 'Please input your username!' }]}>
+        <Input placeholder="Username" size="large" prefix={<UserOutlined />}></Input>
       </Form.Item>
-      <Form.Item label="Email address:" name="email">
+      <Form.Item
+        label="Email address:"
+        name="email"
+        rules={[
+          { type: 'email', message: 'The input is not valid Email!' },
+          { required: true, message: 'Please input your Email!' },
+        ]}
+      >
         <Input placeholder="Email address" size="large" prefix={<MailOutlined />} />
       </Form.Item>
-      <Form.Item label="Password:" name="password">
+      <Form.Item
+        label="Password:"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+        hasFeedback
+      >
         <Input.Password placeholder="Password" size="large" prefix={<LockOutlined />} />
+      </Form.Item>
+      <Form.Item
+        label="Repeat Password:"
+        name="confirm"
+        dependencies={['password']}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: 'Please confirm your password!',
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(new Error('The new password that you entered do not match!'));
+            },
+          }),
+        ]}
+      >
+        <Input.Password placeholder="Repeat password" size="large" prefix={<UnlockOutlined />} />
+      </Form.Item>
+      <Form.Item
+        name="agreement"
+        valuePropName="checked"
+        rules={[
+          {
+            validator: (_, value) =>
+              value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement!')),
+          },
+        ]}
+      >
+        <Checkbox>I agree to the processing of my personal information</Checkbox>
       </Form.Item>
       <Form.Item>
         <Button className={styles.Button} type="primary" htmlType="submit" block size="large">
-          Login
+          Create
         </Button>
         <span className={styles.ButtonSpan}>
-          Don&apos;t have an account? <Link to="/sign-up">Sign Up</Link>
+          Already have an account? <Link to="/sign-in">Sign In</Link>
         </span>
       </Form.Item>
     </Form>
